827b02586050518b60af97169936cd8fa96dfbe0
Everezze <83906211+Everezze@users.noreply.github.com>
2022-09-03 21:07:04 +0200

    checking rows,columns and diagonals for win should work now without errors

--- utils.py
+++ utils.py
@@ -109,25 +109,8 @@ def check_win(list_of_lists, dimension):
     dimension -=1
     x=0
     column=0
-    #diagonal=0
-    #tile_checked=1
-
-    #while True:
-
-    #if x ==dimension then all are crossed meaning player won, check for 0 case
-    #if x == dimension:
-        #print(f'player {list_of_lists[dimension-x][column]} won!')
-        #break
-        #column+=1
-        #x=0
-
-    #if column == dimension:
-        #break
-
-    #if diagonal == dimension:
-        #print(f'player {list_of_lists[dimension-x][column]} won!')
-        #break
         
+    #CHECKING FOR ROWS
     for list in list_of_lists:
         if all(move=='x' for move in list):
             return 'x'
@@ -135,49 +118,60 @@ def check_win(list_of_lists, dimension):
             return 'o'
 
     #CHECKING FOR COLUMNS 
-    while column != dimension :
+    while column <= dimension and x != dimension :
 
-        while list_of_lists[dimension-x][column] == list_of_lists[dimension-x-1][column]:
+        if list_of_lists[dimension-x][column] == list_of_lists[dimension-x-1][column]:
+            
             if list_of_lists[dimension-x][column] !=' ':
-                if x == dimension:
-                    #print(f'player {list_of_lists[dimension-x][column]} won!')
-                    return list_of_lists[dimension-x][column]
-                    #break
-            x+=1
+                x+=1
                 #tile_checked +=1
-        x=0
-        column+=1
+            else:
+                x=0
+                column+=1
+
+    if x==dimension:
+        return list_of_lists[dimension-x][column]  
+
+    x=0
+    column=0
 
 
     #CHECKING FOR THE DIAGONALS
-    while list_of_lists[dimension-x][x] == list_of_lists[dimension-x-1][x+1]:
-        if list_of_lists[dimension-x][x] !=' ':
-            if x == dimension:
-                #print(f'player {list_of_lists[dimension-x][x]} won!')
-                return list_of_lists[dimension-x][x]
-                #break
-        x+=1
+    while x != dimension :
+        if list_of_lists[dimension-x][x] == list_of_lists[dimension-x-1][x+1] and  list_of_lists[dimension-x][x] !=' ':
+            x+=1
+        else:
+            break
+
+    if x == dimension:
+        #print(f'player {list_of_lists[dimension-x][x]} won!')
+        return list_of_lists[dimension-x][x]
+        #break
 
     x=0
 
-    while list_of_lists[dimension-x][-1-x] == list_of_lists[dimension-x-1][-1+(x+1)]:
-        if list_of_lists[dimension-x][-1-x] !=' ':
-            if x == dimension:
-                #print(f'player {list_of_lists[dimension-x][-1-x]} won!')
-                return list_of_lists[dimension-x][x-1-x]
-                #break
-        x+=1
+    while x != dimension :
+        if list_of_lists[dimension-x][-1-x] == list_of_lists[dimension-x-1][-1-(x+1)] and list_of_lists[dimension-x][-1-x] !=' ':
+            x+=1
+        else:
+            break
 
-    x=0
+    if x == dimension:
+        #print(f'player {list_of_lists[dimension-x][-1-x]} won!')
+        return list_of_lists[dimension-x][x-1-x]
+        #break
+
+    #x=0
 
 
     for row in list_of_lists:
-        if all(row):
+        if all(char != ' ' for char in row):
             continue
         else:
             return False
     
     return 'draw'
+
 
 
 def replay(answer):
